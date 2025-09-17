593f4eecd91734f2d4febeae41b9bf32af9da489
JBak <jbak2588@gmail.com>
2025-09-17 08:03:56 +0700

    ios 테스트를 위한 코드변경과 gitignore변경 commit

---
+++ lib/api_keys.dart
@@ -0,0 +1,10 @@
+// lib/api_keys.dart
+
+//  const String googleApiKey = "AIzaSyCGr0wgQ67ezSwO5pLowMx6J8emksSaemo"; // 빠몽님의 실제 API 키를 여기에 붙여넣어 주세요.
+
+class ApiKeys {
+  static const googleApiKey =
+      'AIzaSyCGr0wgQ67ezSwO5pLowMx6J8emksSaemo'; // ← API 키 4개에 해당하는 키 입력
+}
+
+
