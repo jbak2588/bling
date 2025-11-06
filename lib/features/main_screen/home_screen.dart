ddc662ffbd616671959e98647fe3e27c775cf95f
JBak <jbak2588@gmail.com>
2025-11-06 11:52:08 +0700

    아이콘 svg 충돌 문제 계속 파일 3d deep 으로 교체

--- lib/features/main_screen/home_screen.dart
+++ lib/features/main_screen/home_screen.dart
@@ -256,9 +256,7 @@ class HomeScreen extends StatelessWidget {
         screen: const Placeholder()),
     // 7. findFriends (친구찾기)
     MenuItem(
-        // svg: '$_iconsPath/ico_friend_theme_combo.svg',
-        // svg: '$_iconsPath/ico_friend_theme_combo_peachPerson.svg',
-        svg: '$_iconsPath/ico_friend.svg',
+        svg: '$_iconsPath/ico_friend_3d_deep.svg',
         labelKey: 'main.tabs.findFriends',
         screen: const FindFriendsScreen()),
     // 8. realEstate (부동산)
